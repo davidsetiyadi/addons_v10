d6a33f4e43487b9eae7c9abb4350a0c48a637191
david <davidsetiadi11@gmail.com>
2018-06-05 09:01:58 +0700

    update fix bugs

--- sale_oplos/wizard/pk.py
+++ sale_oplos/wizard/pk.py
@@ -9,15 +9,24 @@ class efaktur_pk_wizard(models.TransientModel):
 
 
     def baris6(self, headers, csvwriter, line):
-        harga_total = line.oplos_template_id.list_price * line.quantity
+        if line.oplos_template_id:
+            harga_total = line.oplos_template_id.list_price * line.quantity
+            KD_JENIS_TRANSAKSI = line.oplos_template_id.ace_code or ''
+            FG_PENGGANTI = line.oplos_template_id.name_get()[0][1] or ''
+            NOMOR_FAKTUR = line.oplos_template_id.list_price
+        else:
+            harga_total = line.price_unit * line.quantity
+            KD_JENIS_TRANSAKSI = line.product_id.default_code or ''
+            FG_PENGGANTI = line.product_id.name or ''
+            NOMOR_FAKTUR = line.price_unit
         dpp = harga_total
         ppn = dpp*0.1 #TODO ambil dari Tax many2many
 
         data = {
             'FK': 'OF',
-            'KD_JENIS_TRANSAKSI': line.oplos_template_id.ace_code or '',
-            'FG_PENGGANTI': line.oplos_template_id.name_get()[0][1] or '',
-            'NOMOR_FAKTUR': line.oplos_template_id.list_price,
+            'KD_JENIS_TRANSAKSI': KD_JENIS_TRANSAKSI,
+            'FG_PENGGANTI': FG_PENGGANTI,
+            'NOMOR_FAKTUR': NOMOR_FAKTUR,
             'MASA_PAJAK': line.quantity ,
             'TAHUN_PAJAK': harga_total,
             'TANGGAL_FAKTUR': line.discount or 0,
